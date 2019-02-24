11e0776a85a177b8470b1c1358f5d41127e17d99
JagannathT <47922591+JagannathT@users.noreply.github.com>
2019-02-24 14:04:45 +0000

    Delete ASG.tf

--- ASG.tf
+++
@@ -1,9 +0,0 @@
-/*
-resource "aws_autoscaling_group" "ad_asg" {
-  max_size = 2
-  min_size = 2
-  launch_configuration = "${aws_launch_configuration.ad-asg-lc.id}"
-  availability_zones = ["us-west-2a"]
-  vpc_zone_identifier = ["${aws_subnet.us-west-2a.id}"]
-}
-*/
